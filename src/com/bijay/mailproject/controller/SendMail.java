736d6ccd975a300737f8437f54cec78d16e0dd59
dbijay <dbijaythapa@gmail.com>
2019-09-21 19:50:50 +0545

    first commit

---
+++ src/com/bijay/mailproject/controller/SendMail.java
@@ -0,0 +1,38 @@
+package com.bijay.mailproject.controller;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
+public class SendMail {
+
+	/**
+	 * Outgoing Mail (SMTP) Server requires TLS or SSL: smtp.gmail.com (use
+	 * authentication) Use Authentication: Yes Port for TLS/STARTTLS: 587
+	 */
+	public static void main(String[] args) {
+		final String fromEmail = "khumkhatri10@gmail.com"; // requires valid gmail id
+		final String password = "Wayne2016"; // correct password for gmail id
+		final String toEmail = "khumkhatri11@gmail.com"; // can be any email id
+
+		System.out.println("TLSEmail Start");
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com"); // SMTP Host
+		props.put("mail.smtp.port", "587"); // TLS Port
+		props.put("mail.smtp.auth", "true"); // enable authentication
+		props.put("mail.smtp.starttls.enable", "true"); // enable STARTTLS
+
+		// create Authenticator object to pass in Session.getInstance argument
+		Authenticator auth = new Authenticator() {
+			// override the getPasswordAuthentication method
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(fromEmail, password);
+			}
+		};
+		Session session = Session.getInstance(props, auth);
+
+		MailUtil.sendEmail(session, toEmail, "TLSEmail Testing Subject", "Hello Khum Your OTP is 1234");
+	}
+}
